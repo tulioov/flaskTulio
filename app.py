9185f79d08d53005e878622f63594091a7169ec6
Tulio <tulioov@gmail.com>
2023-04-13 00:46:54 -0300

    00:46

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Alo mundo Tulio 00:45</p></h1>'
+    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Alo mundo Tulio 00:46</p></h1>'
 
 @app.route('/api')
 def hello():
